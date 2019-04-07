b91a847ffba8836cb48e4fe6c5352c250ec22662
Nyuks85 <jorgejg@iMac-de-Jorge.local>
2019-04-07 12:02:07 +0200

    Input to alert
    Show in an alert the information of an input and delete by button

---
+++ app.js
@@ -0,0 +1,10 @@
+function Add()
+{
+    var write = document.getElementById("InputText").value;
+        alert(write);
+}
+
+function Clean()
+{
+    document.getElementById("InputText").value = "";
+}
